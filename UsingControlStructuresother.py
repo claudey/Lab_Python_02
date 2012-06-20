3ce3f2799b76d422d7c83a255701739ea4a4b5f8
claudey <clauditey@gmail.com>
2012-06-20 15:52:58 +0000

    chages made up to question 9

--- UsingControlStructuresother.py
+++ UsingControlStructuresother.py
@@ -9,17 +9,21 @@ else:
 
 primarySchoolAge = 6
 votingAge = 18
-presidentAge = 40
-retirementAge = 60
+presidentAge = raw_input("Please enter the age at which someone becomes a president")
+retirementAge = raw_input("Please enter the retirement age")
 personsAge = input("Enter an age: ")
+intPrimarySchoolAge = int(primarySchoolAge)
+intVotingAge = int(votingAge)
+intPresidentAge = int(presidentAge)
+intRetirementAge = int(retirementAge)
 intPersonsAge = int(personsAge)
-if intPersonsAge < primarySchoolAge:
+if intPrimarySchoolAge < 6:
     print "You are too young for school"
-if intPersonsAge >= votingAge:
+elif intVotingAge >= 18:
     print "Remember to vote"
-if intPersonsAge >= presidentAge:
+elif intPresidentAge >= 40:
     print "Vote for me"
-if intPersonsAge < presidentAge:
+elif intPresidentAge < 40:
     print "You can't be President"
-if intPersonsAge >= retirementAge:
+elif intRetirementAge >=60:
     print "Too old."
