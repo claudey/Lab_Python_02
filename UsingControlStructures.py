629061c7a463e261460acb3a551a202887bd11bd
claudey <clauditey@gmail.com>
2012-06-20 16:47:18 +0000

    completed question 9

--- UsingControlStructures.py
+++ UsingControlStructures.py
@@ -5,7 +5,7 @@ if intInput % 2 == 0:
 else:
     print "The number you entered is odd"
 
-"-------------------"
+print "\n-----------------------------------------\n"
 
 primarySchoolAge = 6
 votingAge = 18
@@ -23,3 +23,34 @@ if intPersonsAge < presidentAge:
     print "You can't be President"
 if intPersonsAge >= retirementAge:
     print "Too old."
+
+print "\n-----------------------------------------\n"
+
+i = 40
+if i % 3 !=0:
+    i=i-1
+if i % 3 !=0:
+    i=i-1
+while i % 3 == 0 and i >= 0:
+    print i
+    i -= 3
+
+print "\n-----------------------------------------\n"
+
+i = 6
+while  i < 30:
+    i += 1
+    if i % 2 != 0 and i % 3 != 0 and i % 5 != 0:
+        print i
+
+print "\n-----------------------------------------\n"
+
+n = 1
+x = 79
+while n > 0:    
+    n += 1
+    x = 79 * n
+    if x % 97 == 1:
+        print "The number is",n
+        break
+
